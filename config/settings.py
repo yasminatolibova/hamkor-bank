92a5e28b8745750f214556dc8ec5b7e88df0a2cb
yasminatolibova <yasminatolibova837@gmail.com>
2026-03-13 09:17:14 +0500

    update

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['teamwork-1-oqhf.onrender.com', 'www.teamwork-1-oqhf.onrender.com']
 
 
 # Application definition
